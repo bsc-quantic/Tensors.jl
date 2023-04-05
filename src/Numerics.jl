b526e7de365486ce1302240c84c2d76552111f00
Jofre Vallès <jofrevalles99@gmail.com>
2023-04-05 11:36:57 +0200

    Small fix from last commit

--- src/Numerics.jl
+++ src/Numerics.jl
@@ -59,7 +59,7 @@ function LinearAlgebra.svd(t::Tensor, left_inds=(); kwargs...)
     # tensorify results
     U = reshape(U, size.((t,), left_inds)..., size(U, 2))
     s = Diagonal(s)
-    Vt = reshape(V', size.((t,), right_inds)..., size(Vt, 2))
+    Vt = reshape(V', size.((t,), right_inds)..., size(V, 2))
 
     vlind = Symbol(uuid4())
     vrind = Symbol(uuid4())

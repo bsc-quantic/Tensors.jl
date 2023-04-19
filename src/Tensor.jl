1bca8eff94657e378e60aeb4823f5d5baaca9882
Jofre Vallès <jofrevalles99@gmail.com>
2023-04-19 13:04:50 +0200

    Add transpose function for one and two-dimension Tensor

--- src/Tensor.jl
+++ src/Tensor.jl
@@ -153,3 +153,9 @@ function Base.view(t::Tensor, inds::Pair{Symbol,<:Any}...)
 end
 
 Base.adjoint(t::Tensor) = Tensor(conj(parent(t)), labels(t); t.meta...)
+
+Base.transpose(t::Tensor{T,1,A}) where {T, A<:AbstractArray{T, 1}} =
+    permutedims(t, (1,))
+
+Base.transpose(t::Tensor{T,2,A}) where {T, A<:AbstractArray{T, 2}} =
+    permutedims(t, (2, 1))

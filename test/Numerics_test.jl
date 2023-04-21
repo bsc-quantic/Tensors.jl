2bcd3575d5821c1d3afc3d8c38348be4c9769f42
Jofre Vallès <jofrevalles99@gmail.com>
2023-04-21 12:43:18 +0200

    Add tests for contract function

--- test/Numerics_test.jl
+++ test/Numerics_test.jl
@@ -47,4 +47,75 @@
             @test U2 * s2 * V2 ≈ tensor2
         end
     end
+
+    @testset "contract" begin
+        using Tensors: Tensor, contract
+
+        @testset "Basic Functionality" begin
+            A = Tensor(rand(2, 3), (:i, :j))
+            B = Tensor(rand(3, 4), (:j, :k))
+
+            C = contract(A, B)
+            C_mat =
+            @test labels(C) == (:i, :k)
+            @test size(C) == (2, 4)
+            @test C ≈ A * B ≈ A.data * B.data
+        end
+
+        @testset "Outer product test" begin
+            A = Tensor(rand(2, 2), (:i, :j))
+            B = Tensor(rand(2, 2), (:k, :l))
+
+            C = contract(A, B)
+            C_ein = ein"ij, kl -> ijkl"(A, B)
+            @test size(C) == (2, 2, 2, 2) == size(C_ein)
+            @test labels(C) == (:i, :j, :k, :l)
+            @test C ≈ C_ein
+        end
+
+        @testset "Scalar and Tensor multiplication" begin
+            A = Tensor(rand(2, 2), (:i, :j))
+            scalar = 2.0
+
+            C = contract(A, scalar)
+            @test labels(C) == (:i, :j)
+            @test size(C) == (2, 2)
+            @test C ≈ A.data * scalar
+
+            D = contract(scalar, A)
+            @test labels(D) == (:i, :j)
+            @test size(D) == (2, 2)
+            @test D ≈ scalar * A.data
+        end
+
+        @testset "Contraction Over Specific Indices" begin
+            A = Tensor(rand(2, 3, 4), (:i, :j, :k))
+            B = Tensor(rand(4, 5, 3), (:k, :l, :j))
+
+            # Contraction of all common indices
+            C = contract(A, B, (:j, :k))
+            C_ein = ein"ijk, klj -> il"(A, B)
+            @test labels(C) == (:i, :l)
+            @test size(C) == (2, 5) == size(C_ein)
+            @test C ≈ C_ein
+
+            # Contraction of not all common indices
+            C = contract(A, B, (:j,))
+            C_ein = ein"ijk, klj -> ikl"(A, B)
+            @test labels(C) == (:i, :k, :l)
+            @test size(C) == (2, 4, 5) == size(C_ein)
+            @test C ≈ C_ein
+        end
+
+        @testset "Complex Contraction" begin
+            A = Tensor(rand(Complex{Float64}, 2, 3, 4), (:i, :j, :k))
+            B = Tensor(rand(Complex{Float64}, 4, 5, 3), (:k, :l, :j))
+
+            C = contract(A, B, (:j, :k))
+            C_ein = ein"ijk, klj -> il"(A, B)
+            @test labels(C) == (:i, :l)
+            @test size(C) == (2, 5) == size(C_ein)
+            @test C ≈ C_ein
+        end
+    end
 end

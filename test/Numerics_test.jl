adba38341e540fde8c8004d6dc4ae25c397bb482
Jofre Vallès Muns <61060572+jofrevalles@users.noreply.github.com>
2023-04-21 17:05:24 +0200

    Test `contract` methods (#18)
    * Add tests for contract function
    
    * Small fix from last commit
    
    * Change names of test_set
    
    * Add OMEinsum as test dependency
    
    * Use OMEinsum
    
    * Rename `contract` test sets
    
    * Insert `contract`'s "Complex Numbers" test set into "manual" test set
    
    * Fix `OMEinsum` import error on tests
    
    * Add "inner product" test to `contract` test set
    
    * Use `parent` instead of `.data`
    
    * Fix `LinearAlgebra` import in "Numerics" test set
    
    ---------
    
    Co-authored-by: Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>

--- test/Numerics_test.jl
+++ test/Numerics_test.jl
@@ -1,9 +1,8 @@
 @testset "Numerics" begin
     using Tensors: Tensor
+    using LinearAlgebra
 
     @testset "svd" begin
-        using LinearAlgebra
-
         data = rand(2, 2, 2)
         tensor = Tensor(data, (:i, :j, :k))
 
@@ -45,6 +44,88 @@
             tensor2 = Tensor(data2, (:i, :j, :k, :l))
             U2, s2, V2 = svd(tensor2, labels(tensor2)[1:2])
             @test U2 * s2 * V2 ≈ tensor2
+        end
+    end
+
+    @testset "contract" begin
+        using Tensors: Tensor, contract
+
+        @testset "matrix multiplication" begin
+            A = Tensor(rand(2, 3), (:i, :j))
+            B = Tensor(rand(3, 4), (:j, :k))
+
+            C = contract(A, B)
+            C_mat = parent(A) * parent(B)
+            @test labels(C) == (:i, :k)
+            @test size(C) == (2, 4) == size(C_mat)
+            @test C ≈ A * B ≈ C_mat
+        end
+
+        @testset "inner product" begin
+            A = Tensor(rand(3, 4), (:i, :j))
+            B = Tensor(rand(4, 3), (:j, :i))
+
+            C = contract(A, B)
+            C_res = LinearAlgebra.tr(parent(A) * parent(B))
+            @test labels(C) == ()
+            @test size(C) == () == size(C_res)
+            @test only(C) ≈ C_res
+        end
+
+        @testset "outer product" begin
+            A = Tensor(rand(2, 2), (:i, :j))
+            B = Tensor(rand(2, 2), (:k, :l))
+
+            C = contract(A, B)
+            C_ein = ein"ij, kl -> ijkl"(A, B)
+            @test size(C) == (2, 2, 2, 2) == size(C_ein)
+            @test labels(C) == (:i, :j, :k, :l)
+            @test C ≈ C_ein
+        end
+
+        @testset "scale" begin
+            A = Tensor(rand(2, 2), (:i, :j))
+            scalar = 2.0
+
+            C = contract(A, scalar)
+            @test labels(C) == (:i, :j)
+            @test size(C) == (2, 2)
+            @test C ≈ parent(A) * scalar
+
+            D = contract(scalar, A)
+            @test labels(D) == (:i, :j)
+            @test size(D) == (2, 2)
+            @test D ≈ scalar * parent(A)
+        end
+
+        @testset "manual" begin
+            A = Tensor(rand(2, 3, 4), (:i, :j, :k))
+            B = Tensor(rand(4, 5, 3), (:k, :l, :j))
+
+            # Contraction of all common indices
+            C = contract(A, B, (:j, :k))
+            C_ein = ein"ijk, klj -> il"(A, B)
+            @test labels(C) == (:i, :l)
+            @test size(C) == (2, 5) == size(C_ein)
+            @test C ≈ C_ein
+
+            # Contraction of not all common indices
+            C = contract(A, B, (:j,))
+            C_ein = ein"ijk, klj -> ikl"(A, B)
+            @test labels(C) == (:i, :k, :l)
+            @test size(C) == (2, 4, 5) == size(C_ein)
+            @test C ≈ C_ein
+
+            @testset "Complex numbers" begin
+                A = Tensor(rand(Complex{Float64}, 2, 3, 4), (:i, :j, :k))
+                B = Tensor(rand(Complex{Float64}, 4, 5, 3), (:k, :l, :j))
+
+                C = contract(A, B, (:j, :k))
+                C_ein = ein"ijk, klj -> il"(A, B)
+                @test labels(C) == (:i, :l)
+                @test size(C) == (2, 5) == size(C_ein)
+                @test C ≈ C_ein
+            end
         end
     end
 end

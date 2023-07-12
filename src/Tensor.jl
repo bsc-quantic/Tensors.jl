a7868954e44dfa1fe0acaaa4f68a225f534bee79
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-07-12 23:53:36 +0200

    Refactor `transpose` on matrices

--- src/Tensor.jl
+++ src/Tensor.jl
@@ -219,5 +219,4 @@ Base.adjoint(t::Tensor) = Tensor(conj(parent(t)), labels(t); t.meta...)
 
 # NOTE: Maybe use transpose for lazy transposition ?
 Base.transpose(t::Tensor{T,1,A}) where {T,A<:AbstractArray{T,1}} = permutedims(t, (1,))
-
-Base.transpose(t::Tensor{T,2,A}) where {T,A<:AbstractArray{T,2}} = permutedims(t, (2, 1))
+Base.transpose(t::Tensor{T,2,A}) where {T,A<:AbstractArray{T,2}} = Tensor(transpose(parent(t)), reverse(labels(t)); t.meta...)
